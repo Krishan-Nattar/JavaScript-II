968dd17c57fdadc888d8c70cbd4ed22561d787e0
Krishan Nattar <krishan.nattar@gmail.com>
2019-07-19 14:57:29 -0700

    Closure challenge 1 finished.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function myNameFunction(firstName){
+  
+  function insideNameFunction(lastName){
+    
+    console.log(firstName + " " + lastName);
+  }
+  return insideNameFunction;
+}
+
+const myName = myNameFunction("Krishan");
+myName("Nattar");
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
